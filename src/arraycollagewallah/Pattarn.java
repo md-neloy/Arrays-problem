eab36e8f90c798643e0ed0c5b46959759bcef027
mh neloy <mhneloy708@gmail.com>
2024-05-10 18:41:35 +0600

    pattern pb

--- src/arraycollagewallah/Pattarn.java
+++ src/arraycollagewallah/Pattarn.java
@@ -1,31 +1,20 @@
 
 package arraycollagewallah;
-
 public class Pattarn {
     public static void main(String[] args) {
         int n = 4;
-        for (int i = 1; i <=n; i++) {
-            char ch = 64;
-            for (int j = 1; j <=n-i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <=2*i-1; j++) {
-                if(j<=i){
-                    ch++;
-                    System.out.print(ch);
-                    
-                }else{
-                    ch--;
-                    System.out.print(ch);
-                }
+        for (int i = 1; i <=2*n-1; i++) {
+            for(int j = 1; j<= 2*n-1; j++){
+                int a = i;
+                if(i>=n) a = 2*n-i;
+                int b = j;
+                if(b>=n) b = 2*n-j;
+                if(a<b) System.out.print(n+1-a);
+                else System.out.print(n+1-b);
+                
             }
             System.out.println();
         }
-        StringBuilder st = new StringBuilder("hello");
-        String s = "hello";
-        char ch = s.charAt(0);
-        char low = Character.toUpperCase(ch);
-            s += low;
-            System.out.println(s.substring(3,4));
+        
     }
 }
